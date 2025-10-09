6f62d4313ad56143ffc3ac063578906ee6ff3f28
RemoteMS <rostov.90.100@gmail.com>
2025-10-09 12:59:24 +0400

    card hand animation

--- Assets/_Project/Src/Common/HandStack/FirstCardsInHadView.cs
+++ Assets/_Project/Src/Common/HandStack/FirstCardsInHadView.cs
@@ -1,96 +1,182 @@
+using System;
 using _Project.Src.Common.CellDatas.Settings;
 using _Project.Src.Common.Hex;
 using _Project.Src.Common.PlayerInputs.Storages;
+using LitMotion;
+using LitMotion.Extensions;
 using UniRx;
 using UnityEngine;
 using VContainer;
 
 namespace _Project.Src.Common.HandStack
 {
-    public class FirstCardsInHadView : MonoBehaviour
+    public class FirstCardsInHadView : MonoBehaviour, IDisposable
     {
-        [SerializeField] private HexView firstCardView;
-        private CellController _firstCardController;
+        [Serializable]
+        public class ElementView : IDisposable
+        {
+            public Transform viewTransform => cardView.gameObject.transform;
+            [SerializeField] private HexView cardView;
 
-        [SerializeField] private HexView secondCardView;
-        private CellController _secondController;
+            private CellController _cardController;
 
-        [SerializeField] private HexView thirdCardView;
-        private CellController _thirdController;
+            public Transform cardTargetTransform => _cardTargetTransform;
+            private Transform _cardTargetTransform;
+
+            public void InitTargetPoint(Transform parent, int index, float heightOffset)
+            {
+                var pointTarget = new GameObject($"pointTarget_{index}");
+                pointTarget.transform.parent = parent;
+                pointTarget.transform.localPosition = Vector3.down * (index * heightOffset);
+                _cardTargetTransform = pointTarget.transform;
+
+                cardView.transform.localPosition = pointTarget.transform.localPosition;
+            }
+
+            public void InitController(CellModel cellModel, CellSettings settings)
+            {
+                _cardController?.Dispose();
+                _cardController = null;
+
+                if (cellModel != null)
+                {
+                    cardView.EnableRendererContainer();
+                    _cardController = new CellController(cellModel, settings);
+                    _cardController.BindView(cardView);
+                    cardView.Bind(_cardController);
+                }
+                else
+                {
+                    cardView.DisableRendererContainer();
+                }
+            }
+
+            public void SetLocalPosition(Vector3 localPosition)
+            {
+                cardView.gameObject.transform.localPosition = localPosition;
+            }
+
+            public void SetLocalPosition(ElementView elementView)
+            {
+                cardView.gameObject.transform.localPosition = elementView._cardTargetTransform.localPosition;
+            }
+
+            public void Dispose()
+            {
+                _cardController?.Dispose();
+            }
+        }
+
+        [SerializeField] private bool moveHexes = true;
+
+
+        [SerializeField] private ElementView firstEl;
+        [SerializeField] private ElementView secondEl;
+        [SerializeField] private ElementView thirdEl;
+
 
         [SerializeField] private float heightOffset = 3f;
         private readonly CompositeDisposable _disposables = new();
 
-        [SerializeField] private bool moveHexes = true;
 
         [Inject]
         public void Inject(PlayerInputStorage storage, CellSettings settings)
         {
-            if (moveHexes)
-                InitializePositions();
+            InitializePositions();
 
             storage.currentCellModelInHand
-                .Subscribe(x => HandleCellModel(x, firstCardView, settings, ref _firstCardController))
+                .Subscribe(cellModel => { firstEl.InitController(cellModel, settings); })
                 .AddTo(_disposables);
 
             storage.secondCellModelInHand
-                .Subscribe(x => HandleCellModel(x, secondCardView, settings, ref _secondController))
+                .Subscribe(cellModel => { secondEl.InitController(cellModel, settings); })
                 .AddTo(_disposables);
 
             storage.thirdCellModelInHand
-                .Subscribe(x => HandleCellModel(x, thirdCardView, settings, ref _thirdController))
+                .Subscribe(cellModel => { thirdEl.InitController(cellModel, settings); })
+                .AddTo(_disposables);
+
+
+            // animations
+
+            storage.currentCellModelInHand
+                .Subscribe(_ => HandleFirstCardEvent())
+                .AddTo(_disposables);
+
+            storage.secondCellModelInHand
+                .Subscribe(_ => HandleSecondCardEvent())
                 .AddTo(_disposables);
         }
 
-        private void InitializePositions()
+        private MotionHandle _currentAnimationHandle;
+        private MotionHandle _secondAnimationHandle;
+
+        [SerializeField] private float timeDuration = 1f;
+
+        private void HandleFirstCardEvent()
         {
-            if (firstCardView)
+            if (_currentAnimationHandle.IsActive())
             {
-                firstCardView.transform.localPosition = Vector3.down * (0 * heightOffset);
+                _currentAnimationHandle.Cancel();
             }
 
+            firstEl.SetLocalPosition(secondEl);
 
-            if (secondCardView && secondCardView)
-            {
-                secondCardView.transform.localPosition = Vector3.down * (1 * heightOffset);
-            }
-
-
-            if (thirdCardView)
-            {
-                thirdCardView.transform.localPosition = Vector3.down * (2 * heightOffset);
-            }
+            _currentAnimationHandle = LMotion
+                .Create(secondEl.cardTargetTransform.localPosition, firstEl.cardTargetTransform.localPosition,
+                    timeDuration)
+                .WithEase(Ease.OutCubic)
+                .BindToLocalPosition(firstEl.viewTransform)
+                .AddTo(this);
         }
 
-        private void HandleCellModel(
-            CellModel cellModel,
-            HexView view,
-            CellSettings settings,
-            ref CellController controller
-        )
+        private void HandleSecondCardEvent()
         {
-            controller?.Dispose();
-            controller = null;
+            if (_secondAnimationHandle.IsActive())
+            {
+                _secondAnimationHandle.Cancel();
+            }
 
-            if (cellModel != null)
-            {
-                view.EnableRendererContainer();
-                controller = new CellController(cellModel, settings);
-                controller.BindView(view);
-                view.Bind(controller);
-            }
-            else
-            {
-                view.DisableRendererContainer();
-            }
+            secondEl.SetLocalPosition(thirdEl);
+
+
+            _secondAnimationHandle = LMotion
+                .Create(thirdEl.cardTargetTransform.localPosition, secondEl.cardTargetTransform.localPosition,
+                    timeDuration)
+                .WithEase(Ease.OutCubic)
+                .BindToLocalPosition(secondEl.viewTransform)
+                .AddTo(this);
+        }
+
+
+        private void InitializePositions()
+        {
+            firstEl.InitTargetPoint(gameObject.transform, 0, heightOffset);
+            secondEl.InitTargetPoint(gameObject.transform, 1, heightOffset);
+            thirdEl.InitTargetPoint(gameObject.transform, 2, heightOffset);
         }
 
         private void OnDestroy()
         {
-            _disposables.Dispose();
-            _firstCardController?.Dispose();
-            _secondController?.Dispose();
-            _thirdController?.Dispose();
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (_currentAnimationHandle.IsActive())
+            {
+                _currentAnimationHandle.Cancel();
+            }
+
+            if (_secondAnimationHandle.IsActive())
+            {
+                _secondAnimationHandle.Cancel();
+            }
+
+            firstEl?.Dispose();
+            secondEl?.Dispose();
+            thirdEl?.Dispose();
+            _disposables?.Dispose();
         }
     }
 }
